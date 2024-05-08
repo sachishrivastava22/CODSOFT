db5c853f6a88b6e591cfe6235b192d382c2de899
sachishrivastava22 <127180412+sachishrivastava22@users.noreply.github.com>
2024-05-08 18:55:05 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> op;
+
+    switch(op) {
+        case '+':
+            cout << "Result: " << num1 + num2;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                cout << "Result: " << num1 / num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+        default:
+            cout << "Invalid operation!";
+    }
+
+    return 0;
+}
